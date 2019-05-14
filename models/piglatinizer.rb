8e1471a211e5a852666f38a5f54ffec74760f57f
Richard Yance <yance0022@gmail.com>
2019-05-14 17:44:09 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,28 +1,28 @@
 class PigLatinizer
 
-  def piglatinize(string)
-    arr1 = string.split(" ")
-    arr2 = arr1.map {|string| piglatinize_word(string)}
+  def piglatinize(word)
+    arr1 = word.split(" ")
+    arr2 = arr1.map {|word| piglatinize_word(word)}
     arr2.join(" ")
   end
 
-  def piglatinize_word(string)
-    first_letter = string[0].downcase
+  def piglatinize_word(word)
+    first_letter = word[0].downcase
     if ["a","e","i","o","u"].include?(first_letter)
       "#{word}way"
     else
       consonants = []
-      consonants << string[0]
+      consonants << word[0]
 
-      if ["a","e","i","o","u"].include?(string[1]) == false
-        consonants << string[1]
+      if ["a","e","i","o","u"].include?(word[1]) == false
+        consonants << word[1]
 
-      if ["a","e","i","o","u"].include?(string[2]) == false
-        consonants << string[2]
+      if ["a","e","i","o","u"].include?(word[2]) == false
+        consonants << word[2]
 
       end
     end
-        "#{string[consonants.length..-1] + consonants.join + "ay"}"
+        "#{word[consonants.length..-1] + consonants.join + "ay"}"
     end
   end
 end  
